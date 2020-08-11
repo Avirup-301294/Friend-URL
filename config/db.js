148de71b2ea954f1d80f4d47fb6259c651d4a24c
Avirup-301294 <avirupmondal1994@gmail.com>
2020-08-11 13:38:48 +0530

    config added

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose'),
+      config = require('config'),
+	     db = config.get('mongoURI');
+const connectDB = async () => {
+	try {
+		await mongoose.connect(db, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		});
+		console.log('MongoDB connected');
+	  } catch(err){
+		console.log(err.message);
+		process.exit(1);
+	}
+}
+module.exports = connectDB;
