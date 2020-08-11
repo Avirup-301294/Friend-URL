ee4062b1e6fca2489442efc019d0a6f84af69f83
Avirup-301294 <avirupmondal1994@gmail.com>
2020-08-11 14:29:43 +0530

    config changes

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'),
       config = require('config'),
 	     db = config.get('mongoURI');
        var uri = process.env.MongoURI || db;
